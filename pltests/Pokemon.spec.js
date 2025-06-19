6ecd5bfb2aca1d00b932576853d878c823b4f750
Manjunath Teli <manjunath.teli@hfn.local>
2025-06-19 12:27:13 +0530

    changed the playwrigt configuration port number

--- pltests/Pokemon.spec.js
+++ pltests/Pokemon.spec.js
@@ -3,7 +3,7 @@ import { test, expect, describe } from '@playwright/test';
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('http://localhost:5001/pokemon/ivysaur')
+     await page.goto('http://localhost:8080/pokemon/ivysaur')
     await expect(page.getByText('Ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
